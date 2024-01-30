0df7b6ca4812ee9db4b41f7e9cf3d13243b26951
LucasJara <jaralucas187@gmail.com>
2024-01-30 18:57:31 -0300

    cambiar

--- 03_strings.py
+++ 03_strings.py
@@ -1,6 +1,5 @@
 ### Strings ### 
 
-
 my_string = "Mi String"
 my_other_string = 'Mi otro String'
 
@@ -25,3 +24,33 @@ name, surname, age = "Lucas", "Jara", 36
 print("Mi nombre es {} {} y mi edad es {}".format(name, surname, age))
 print("Mi nombre es %s %s y mi edad es %d" %(name, surname, age))
 print("Mi nombre es " + name + " " + surname + " y mi edad es " + str(age))
+print(f"Mi nombre es {name} {surname} y mi edad es {age}")
+
+# Desempaqueado de caracteres
+language = "Python"
+a, b, c, d, e, f = language
+print(a)
+print(e)
+
+# División
+
+language_slice = language[1:3]
+print(language_slice)
+
+language_slice = language[1:]
+print(language_slice)
+
+language_slice = language[-2]
+print(language_slice)
+
+language_slice = language[0:6:2]
+print(language_slice)
+
+# Reverse
+
+reversed_language = language[::-1]
+print(reversed_language)
+
+# Funciones
+
+print(language.capitalize)
