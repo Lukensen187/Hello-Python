115426fc2f0a398e871106c87c94d0fdaf4f091b
LucasJara <jaralucas187@gmail.com>
2024-01-31 18:28:07 -0300

    05_tuples.py

---
+++ 05_tuples.py
@@ -0,0 +1,16 @@
+### Tuples ###
+
+my_tuples = tuple()
+my_other_tuples = ()
+
+my_tuples = (35, 1.86, "Lucas", "Jara")
+print(my_tuples)
+print(type(my_tuples))
+
+print(my_tuples[0])
+print(my_tuples[-1])
+
+
+
+
+
