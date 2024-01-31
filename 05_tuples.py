a21106d776c6ed2093cf7f674698336378552c5d
LucasJara <jaralucas187@gmail.com>
2024-01-31 20:18:53 -0300

    tuple fin

--- 05_tuples.py
+++ 05_tuples.py
@@ -1,16 +1,38 @@
 ### Tuples ###
 
-my_tuples = tuple()
-my_other_tuples = ()
+my_tuple = tuple()
+my_other_tuple = ()
 
-my_tuples = (35, 1.86, "Lucas", "Jara")
-print(my_tuples)
-print(type(my_tuples))
+my_tuple = (35, 1.86, "Lucas", "Jara", "Lucas")
+my_other_tuple = (35, 60, 30)
 
-print(my_tuples[0])
-print(my_tuples[-1])
+print(my_tuple)
+print(type(my_tuple))
 
+print(my_tuple[0])
+print(my_tuple[-1])
+#print(my_tuples[4]) IndexError
+#print(my_tuples[-6]) IndexError
 
+print(my_tuple.count("Lucas"))
+print(my_tuple.index("Jara"))
+print(my_tuple.index("Lucas"))
 
+#my_tuple[1] = 1.90 Error
 
+my_sum_tuple = my_tuple + my_other_tuple
+print(my_sum_tuple)
 
+print(my_sum_tuple[3:6])
+
+my_tuple = list(my_tuple)
+print(type(my_tuple))
+
+my_tuple[4] = "LucasDev"
+my_tuple.insert(1, "Azul")
+print(tuple(my_tuple))
+print(my_tuple)
+print(type(my_tuple))
+
+del my_tuple
+#print(my_tuple) NameError: name 'my_tuple' is not defined
