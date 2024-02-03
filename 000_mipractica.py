ba642927372c94a09b141671b42ec2d9ed7ad731
LucasJara <jaralucas187@gmail.com>
2024-02-03 18:02:05 -0300

    Loops

--- 000_mipractica.py
+++ 000_mipractica.py
@@ -1,4 +1,14 @@
-# Variables , Definicion , Datos , Elementos () "" {} []
-# my_list   = "Hola mundo" 
+# Reto 01 FizzBuzz #
 
-# print(my_list)
+
+for f in range(1, 101):
+    if f % 3 == 0 and f % 5 == 0:
+        print("fizzbuzz")
+    elif f % 3 == 0:
+        print("fizz") 
+    elif f % 5 == 0:
+        print("buzz")
+    else:
+        print(f)
+
+#
