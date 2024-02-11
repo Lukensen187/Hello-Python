49b8a2677f422f66b6be04268065acf4598deda9
LucasJara <jaralucas187@gmail.com>
2024-02-11 16:32:47 -0300

    sigo practicando

--- 09_loops.py
+++ 09_loops.py
@@ -1,1 +1,20 @@
 ### Loops ###
+
+# While
+
+my_condition = 0
+
+while my_condition < 10:
+    print(my_condition)
+    my_condition += 2
+else: # Es opcional
+    print("Mi condicion es mayor o igual a 10")
+
+print("La ejecución continúa")
+
+while my_condition < 20:
+    my_condition += 2
+    if my_condition == 15:
+        print("Mi condición es 15")
+
+    print("Mi condicion es menos que 20")
